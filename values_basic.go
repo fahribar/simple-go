dab581d3aee0f03f5bc3e56319fdf797fe01bb94
Fakhri Akbar Pratama <fakhri.akbar.pratama@corp.bri.co.id>
2023-09-28 16:26:11 +0700

    Pertamax

---
+++ values_basic.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Test"+"Ing")
+	fmt.Println("Jumlahkan 100*10", 100*10)
+	fmt.Println("What if true AND false", true && false)
+	fmt.Println("What if true OR false", true || false)
+	fmt.Println("What if NOT true", !true)
+}
---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a=100
+	var b=118231
+	fmt.Println("Value of a= ", a)
+	fmt.Println("Value of b= ", b)
+	fmt.Println("Operasi bilangan a dan b ", a*b)
+}
